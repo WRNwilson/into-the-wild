5acce666cb2d04edc061db65de7cdb315630ecd1
Warren Wilson <warrenwilson77@gmail.com>
2021-08-27 15:19:42 -0400

    8. Added a Nav

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 import ServicesSection from "./components/ServicesSection";
+import Nav from "./components/Nav";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
+
       {/* <ServicesSection /> */}
     </div>
   );
