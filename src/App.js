a9735d84bbd2b8368a8457e8e50fb623f3d5a928
Warren Wilson <warrenwilson77@gmail.com>
2021-08-27 07:23:33 -0400

    3. Services Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import ServicesSection from "./components/ServicesSection";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
 function App() {
   return (
     <div className="App">
       <AboutUs />
+      {/* <ServicesSection /> */}
     </div>
   );
 }
