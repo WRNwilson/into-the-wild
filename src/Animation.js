8bc154cd55b3585c3d921cc86b59cb0685eb6137
Warren Wilson <warrenwilson77@gmail.com>
2021-08-30 09:01:54 -0400

    14. Added more animations to about page

--- src/Animation.js
+++ src/Animation.js
@@ -7,47 +7,48 @@ export const pageAnimation = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 1,
+      duration: 0.85,
+      staggerChildren: 0.3,
     },
   },
   exit: {
     opacity: 0,
     y: 300,
     transition: {
-      duration: 1,
+      duration: 0.5,
     },
   },
 };
 
-// export const titleAnim = {
-//   hidden: { y: 200 },
-//   show: {
-//     y: 0,
-//     transition: { type: "tween", ease: "easeOut", duration: 0.75 },
-//   },
-// };
+export const titleAnim = {
+  hidden: { y: 200 },
+  show: {
+    y: 0,
+    transition: { ease: "easeOut", duration: 0.75 },
+  },
+};
 
-// export const fade = {
-//   hidden: { opacity: 0 },
-//   show: {
-//     opacity: 1,
-//     transition: { type: "tween", ease: "easeOut", duration: 1, delay: 0.5 },
-//   },
-// };
+export const fade = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: { ease: "easeOut", duration: 1, delay: 0.75 },
+  },
+};
 
-// export const photoAnim = {
-//   hidden: { scale: 1.5, opacity: 0 },
-//   show: {
-//     scale: 1,
-//     opacity: 1,
-//     transition: {
-//       type: "tween",
-//       ease: "easeOut",
-//       duration: 1,
-//       delay: 0.5,
-//     },
-//   },
-// };
+export const photoAnim = {
+  hidden: { scale: 1.5, opacity: 0 },
+  show: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "tween",
+      ease: "easeOut",
+      duration: 1,
+      delay: 0.5,
+    },
+  },
+};
 
 // export const lineAnim = {
 //   hidden: { width: "0%" },
